b56e66245b04a6a7c19c54f96f98e0b03892527e
Stephen Oliver <steve@infincia.com>
2020-10-24 20:14:05 -0400

    Force install deps

--- install_dep.sh
+++ install_dep.sh
@@ -1,4 +1,4 @@
-apt update || exit 1
+apt -y update || exit 1
 
-apt install autoconf automake libtool pkg-config libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libraspberrypi-dev || exit 1
+apt -y install autoconf automake libtool pkg-config libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libraspberrypi-dev || exit 1
 
